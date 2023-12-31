81c8dc2983c266bbe4aa3ef2f932684a2df36d31
caret <caret@yubi.moe>
2023-12-30 21:42:45 -0600

    update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,6 +9,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar primary -r &
 polybar secondary -r &
-polybar third -r &
 
 echo "Polybar launched..."
