d6843f7a50dbb1cdce807d47a46d1e3a59da7d1a
641i130 <galileoray@gmail.com>
2024-03-22 14:36:40 -0500

    sound

--- configuration.nix
+++ configuration.nix
@@ -86,7 +86,7 @@
 
   # Enable sound with pipewire.
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = true;
   nixpkgs.config.pulseaudio = true;
   hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
   security.rtkit.enable = true;
@@ -94,18 +94,18 @@
     enable = true;
     volumeStep = "5%";
   };
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  #services.pipewire = {
+  #  enable = true;
+  #  alsa.enable = true;
+  #  alsa.support32Bit = true;
+  #  pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
+  #};
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -175,6 +175,7 @@ let
     ark
     mpvScripts.mpris
     mpv
+    mpd
     playerctl
     alsa-utils
     pamixer
